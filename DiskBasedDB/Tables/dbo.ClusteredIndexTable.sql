ed5c48c17ccd4e17b5c4e3c68b57f99c902d0f6b
Frank Gill <fgill@apparatus.net>
2014-04-29 15:57:13 -0500

    Added clustered index to ClusteredIndexTable
    Added clustered index IX_ClusteredIndexTable_ID to ClusteredIndexTable

--- DiskBasedDB/Tables/dbo.ClusteredIndexTable.sql
+++ DiskBasedDB/Tables/dbo.ClusteredIndexTable.sql
@@ -3,4 +3,6 @@ CREATE TABLE [dbo].[ClusteredIndexTable]
 [ID] [int] NOT NULL IDENTITY(1, 1),
 [UserName] [varchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
+CREATE UNIQUE CLUSTERED INDEX [IX_CLusterIndexTable_ID] ON [dbo].[ClusteredIndexTable] ([ID]) ON [PRIMARY]
+
 GO
